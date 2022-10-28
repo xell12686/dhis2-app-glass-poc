05c16f0f11bc8865e7c6333e877e69f9d46c3652
James FL <jlacandula@gmail.com>
2022-10-28 18:46:25 +0800

    Initial commit

---
+++ src/webapp/pages/landing/LandingPage.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Card, CardGrid } from "../../components/card-grid/CardGrid";
+
+export const LandingPage: React.FC = React.memo(() => {
+    const history = useHistory();
+
+    const cards: Card[] = [
+        {
+            title: "Section",
+            key: "main",
+            children: [
+                {
+                    name: "John",
+                    description: "Entry point 1",
+                    listAction: () => history.push("/for/John"),
+                },
+                {
+                    name: "Mary",
+                    description: "Entry point 2",
+                    listAction: () => history.push("/for/Mary"),
+                },
+            ],
+        },
+    ];
+
+    return <CardGrid cards={cards} />;
+});
